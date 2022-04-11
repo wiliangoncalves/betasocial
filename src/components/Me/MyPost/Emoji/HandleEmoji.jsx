f762186b9c6a8c184668782fda8e8e616e51efe3
wilian goncalves <wwiliangoncalves@gmail.com>
2022-04-10 23:00:41 -0300

    go

--- src/components/Me/MyPost/Emoji/HandleEmoji.jsx
+++ src/components/Me/MyPost/Emoji/HandleEmoji.jsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Picker from 'emoji-picker-react';
 
 import "./HandleEmoji.css";
 
 export default function HandleEmoji(props){
-    const [chosenEmoji, setChosenEmoji] = useState(null);
-
     const onEmojiClick = (e, emojiObject) => {
-        setChosenEmoji(emojiObject);
+        document.querySelector(".textArea").value += emojiObject.emoji;
 
         e.preventDefault();
     };
@@ -16,18 +14,9 @@ export default function HandleEmoji(props){
         document.getElementById("EmojiContainer").style.display = "none";
     }
 
-    if(chosenEmoji !== null){
-        console.log("Tem conteúdo!");
-        document.querySelector(".textArea").innerHTML = chosenEmoji.emoji;
-    }else{
-        console.log("Não tem conteúdo!");
-    }
-
     return (
         <div id="EmojiContainer">
             <h2 onClick={HandleEmojiClosed}>Fechar X</h2>
-
-            {chosenEmoji ? ( <span>Você escolheu: {chosenEmoji.emoji}</span>) : (<span>Nenhum emoji selecionado!</span>)}
 
             <Picker onEmojiClick={onEmojiClick} />
         </div>
