102297b132294ea16449d50f0435837daa1bb4f1
wilian gonçalves <wwiliangoncalves@gmail.com>
2022-05-18 15:59:07 -0300

    Navegação com um novo visual

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -17,24 +17,24 @@ export default function Nav(){
 
             <div className="IconsContainer">
 
-            <p>Menu</p>
-                <a href="/"><AiFillHome size={22} className="homeIcon"/>
+                <p>Menu</p>
+                <a href="/"><AiFillHome size={18} className="homeIcon"/>
                     <span className="homeSpan">Home</span>
                 </a>
 
-                <a href="/groups"><BsFillPeopleFill size={22} className="groupsIcon"/>
+                <a href="/groups"><BsFillPeopleFill size={18} className="groupsIcon"/>
                     <span className="groupsSpan">Groups</span>
                 </a>
                 
-                <a href="/news"><IoNewspaperSharp size={22} className="newsIcon" />
+                <a href="/news"><IoNewspaperSharp size={18} className="newsIcon" />
                     <span className="newsSpan">News</span>
                 </a>
 
-                <a href="/about"><IoIosPaper size={22} className="aboutIcon" />
+                <a href="/about"><IoIosPaper size={18} className="aboutIcon" />
                     <span className="aboutSpan">About</span>
                 </a>
                 
-                <a href="/profile"><BsFillPersonFill size={22} className="meIcon" />
+                <a href="/profile"><BsFillPersonFill size={18} className="meIcon" />
                     <span className="profileSpan">Profile</span>
                 </a>
 
@@ -44,12 +44,12 @@ export default function Nav(){
                 <p>Explore</p>
 
                 <a href="/chat">
-                    <BsChatDotsFill size={22} className="chatIcon" />
+                    <BsChatDotsFill size={18} className="chatIcon" />
                     <span className="chatSpan">Beta Chat</span>
                 </a>
 
                 <a href="/apps">
-                    <MdDashboard size={22} className="appsIcon" />
+                    <MdDashboard size={18} className="appsIcon" />
                     <span className="appsSpan">Apps</span>
                 </a>
             </div>
